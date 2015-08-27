5366740105162562e4d1cc9eb40cfb6baaca75f6
Lindsay Wang <tascape@gmail.com>
2015-08-26 17:33:33 -0700

    update ssh prop name var

--- src/main/java/com/tascape/qa/th/comm/SshCommunication.java
+++ src/main/java/com/tascape/qa/th/comm/SshCommunication.java
@@ -38,15 +38,15 @@ import org.slf4j.LoggerFactory;
 public class SshCommunication extends EntityCommunication {
     private static final Logger LOG = LoggerFactory.getLogger(SshCommunication.class);
 
-    public static final String SYSPROP_SSH_HOST = "qa.th.comm.ssh.HOST";
+    public static final String SYSPROP_HOST = "qa.th.comm.ssh.HOST";
 
-    public static final String SYSPROP_SSH_PORT = "qa.th.comm.ssh.PORT";
+    public static final String SYSPROP_PORT = "qa.th.comm.ssh.PORT";
 
-    public static final String SYSPROP_SSH_KEY = "qa.th.comm.ssh.KEY";
+    public static final String SYSPROP_KEY = "qa.th.comm.ssh.KEY";
 
-    public static final String SYSPROP_SSH_USER = "qa.th.comm.ssh.USER";
+    public static final String SYSPROP_USER = "qa.th.comm.ssh.USER";
 
-    public static final String SYSPROP_SSH_PASS = "qa.th.comm.ssh.PASS";
+    public static final String SYSPROP_PASS = "qa.th.comm.ssh.PASS";
 
     private final JSch jSch;
 
