e17f0da4dbef531f40482dcf2be351f1b7975c15
Linsong Wang <tascape@gmail.com>
2018-06-26 10:56:54 -0700

    add some unit test sample

---
+++ src/test/java/com/tascape/reactor/tools/ITXmlHelperTests1.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2015 - present Nebula Bay.
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.tascape.reactor.tools;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+
+public class ITXmlHelperTests1 {
+    private static final Logger LOG = LoggerFactory.getLogger(ITXmlHelperTests1.class);
+
+    @Test
+    public void convertStringToXmlAndBack() throws Exception {
+      String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><project xmlns=\"http://maven.apache.org/POM/4.0.0\"></project>";
+      Document doc = XmlHelper.toXmlDocument(xml);
+      String x = XmlHelper.toXmlString(doc);
+      LOG.info("this is sample integration testing");
+  }
+}
