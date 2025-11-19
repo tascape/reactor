aea971a781c7c743461a458db1206441eca68de3
Linsong Wang <wanglinsong@gmail.com>
2025-11-18 20:59:51 -0800

    fix ddl

--- src/main/resources/db/reactor.sql
+++ src/main/resources/db/reactor.sql
@@ -1,25 +1,6 @@
-/*
-#
-# Copyright (c) 2015 - present Nebula Bay.
-# All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-*/
 
 -- Adminer 5.4.0 MySQL 8.0.40 dump
 
-SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
